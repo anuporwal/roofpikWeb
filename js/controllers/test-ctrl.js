fe6dabe2205bd2f4feca24c1e5304f882264082c
Arpit Mittal <arpit@roofpik.com>
2016-10-07 11:47:47 +0530

    updated project list

--- js/controllers/test-ctrl.js
+++ js/controllers/test-ctrl.js
@@ -1,13 +1,22 @@
-app.controller('testCtrl', function($scope , $mdSidenav) {
-   $scope.close = function () {
-      // Component lookup should always be available since we are not using `ng-if`
-      $mdSidenav('right').close()
-        .then(function () {
-          $log.debug("close RIGHT is done");
-        });
+app.controller('testCtrl', function($scope, $mdSidenav, $timeout) {
+    $scope.close = function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav('right').close()
+            .then(function() {
+                $log.debug("close RIGHT is done");
+            });
     };
 
-    $scope.toggleRight = function(){
-    	$mdSidenav('right').open()
+    $scope.toggleRight = function() {
+        $mdSidenav('right').open()
     }
+
+    $timeout(function(){
+    	console.log('called');
+    	 map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 28.4247649, lng: 76.8496963 },
+        zoom: 12
+    });
+    }, 3000)
+   
 })
