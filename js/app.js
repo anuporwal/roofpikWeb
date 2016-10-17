21d4605c0047b21b705d1858a3ed1553ad70168c
Arpit Mittal <arpit@roofpik.com>
2016-10-17 19:40:55 +0530

    added login signup

--- js/app.js
+++ js/app.js
@@ -1,20 +1,42 @@
 var app = angular.module('NewApp', ['ngMaterial', 'ui.router', 'jkAngularCarousel']);
 var db = firebase.database();
 
+
+app.run(function($rootScope, $timeout) {
+    // localStorage.clear();
+    $rootScope.loginStatus = false;
+    $rootScope.loading = true;
+    firebase.auth().onAuthStateChanged(function(user) {
+        // Sign-out successful.
+        console.log(user);
+        if (user != null) {
+            // User is signed in.
+            $rootScope.loginStatus = true;
+        } else {
+            console.log('called2');
+            $rootScope.loginStatus = false;
+            // No user is signed in.
+        }
+        // An error happened.
+    });
+
+
+});
+
 app.config(['$mdThemingProvider', function($mdThemingProvider) {
 
-   var customBlueMap = $mdThemingProvider.extendPalette('grey', {
-      'contrastDefaultColor': 'light',
-      'contrastDarkColors': ['50'],
-      '50': 'ffffff'
-   });
-   $mdThemingProvider.definePalette('customBlue', customBlueMap);
-   $mdThemingProvider.theme('default')
-   .primaryPalette('customBlue', {
-      'default': '500',
-      'hue-1': '50'
-   })
-   .accentPalette('pink');
-   $mdThemingProvider.theme('input', 'default')
-   .primaryPalette('grey')
+    var customBlueMap = $mdThemingProvider.extendPalette('grey', {
+        'contrastDefaultColor': 'light',
+        'contrastDarkColors': ['50'],
+        '50': 'ffffff'
+    });
+    $mdThemingProvider.definePalette('customBlue', customBlueMap);
+    $mdThemingProvider.theme('default')
+        .primaryPalette('customBlue', {
+            'default': '500',
+            'hue-1': '50'
+        })
+        .accentPalette('pink');
+    $mdThemingProvider.theme('input', 'default')
+        .primaryPalette('grey')
 }]);
--- js/controllers/home-ctrl.js
+++ js/controllers/home-ctrl.js
@@ -1,5 +1,11 @@
-app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
-	// console.log('working');
+app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog) {
+    // console.log('working');
+    $timeout(function() {
+        $('.md-header').fadeIn();
+        $('.home-container').fadeIn();
+        $('.footer').fadeIn();
+
+    }, 2000);
 
     $scope.takeToProjectList = function(param) {
         $state.go('project-list', { from: param });
@@ -7,7 +13,7 @@ app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
 
     $scope.topRated = {};
 
-    if(!checkCookie('familyListObject')){
+    if (!checkCookie('familyListObject')) {
         db.ref('familyList').once('value', function(dataSnapshot) {
             $timeout(function() {
                 $scope.familyListObject = dataSnapshot.val();
@@ -15,7 +21,7 @@ app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
             }, 0);
         })
     }
-    if(!checkCookie('justMarriedListObject')){
+    if (!checkCookie('justMarriedListObject')) {
         db.ref('justMarriedList').once('value', function(dataSnapshot) {
             $timeout(function() {
                 $scope.justMarriedListObject = dataSnapshot.val();
@@ -23,7 +29,7 @@ app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
             }, 0);
         })
     }
-    if(!checkCookie('oldAgeFriendlyListObject')){
+    if (!checkCookie('oldAgeFriendlyListObject')) {
         db.ref('oldAgeFriendlyList').once('value', function(dataSnapshot) {
             $timeout(function() {
                 $scope.oldAgeFriendlyListObject = dataSnapshot.val();
@@ -31,7 +37,7 @@ app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
             }, 0);
         })
     }
-    if(!checkCookie('kidsListObject')){
+    if (!checkCookie('kidsListObject')) {
         db.ref('kidsList').once('value', function(dataSnapshot) {
             $timeout(function() {
                 $scope.kidsListObject = dataSnapshot.val();
@@ -39,7 +45,7 @@ app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
             }, 0);
         })
     }
-    if(!checkCookie('bachelorsListObject')){
+    if (!checkCookie('bachelorsListObject')) {
         db.ref('bachelorsList').once('value', function(dataSnapshot) {
             $timeout(function() {
                 $scope.bachelorsListObject = dataSnapshot.val();
@@ -48,7 +54,7 @@ app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
         })
     }
 
-    if(checkCookie('topRatedObject')){
+    if (checkCookie('topRatedObject')) {
         console.log(JSON.parse(getCookie('topRatedObject')) || {});
         $scope.topRated = JSON.parse((getCookie('topRatedObject')) || {});
         $scope.numProjects = JSON.parse((getCookie('numProjectsObject')) || {});
@@ -75,49 +81,49 @@ app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
         $state.go('project-details', { id: project.projectId, name: project.projectName });
     }
 
-    $scope.openSearch = function(){
+    $scope.openSearch = function() {
         console.log('open search called');
     }
 
     $scope.showAdvanced = function(ev) {
         $mdDialog.show({
-            controller: DialogController,
-            templateUrl: 'templates/search-dialog.html',
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            clickOutsideToClose:true,
-            fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-        })
-        .then(function(answer) {
-            $scope.status = 'You said the information was "' + answer + '".';
-        }, function() {
-            $scope.status = 'You cancelled the dialog.';
-        });
+                controller: DialogController,
+                templateUrl: 'templates/search-dialog.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true,
+                fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+            })
+            .then(function(answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
+            });
     };
 
     function DialogController($scope, $mdDialog) {
         $scope.searchObject = [];
         var count = 0;
-        if(checkCookie('searchObject')){
+        if (checkCookie('searchObject')) {
             console.log(JSON.parse(getCookie('searchObject')) || {});
             var data = JSON.parse((getCookie('searchObject')) || {});
-            angular.forEach(data, function(value, key){
+            angular.forEach(data, function(value, key) {
                 $scope.searchObject.push(value);
             })
         } else {
-            db.ref('search').once('value', function(snapshot){
+            db.ref('search').once('value', function(snapshot) {
                 console.log(snapshot.val());
-                $timeout(function(){
+                $timeout(function() {
                     // $scope.searchObject = snapshot.val();
                     setCookie('searchObject', JSON.stringify(snapshot.val()), 1);
-                    angular.forEach(snapshot.val(), function(value, key){
+                    angular.forEach(snapshot.val(), function(value, key) {
                         count++;
                         $scope.searchObject.push(value);
-                        if(count == Object.keys(snapshot.val()).length){
-                            
+                        if (count == Object.keys(snapshot.val()).length) {
+
                         }
                     })
-                },0);
+                }, 0);
             })
         }
         $scope.hide = function() {
@@ -132,57 +138,24 @@ app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
             $mdDialog.hide(answer);
         };
 
-        $scope.getSearchText = function(){
+        $scope.getSearchText = function() {
             console.log($scope.searchText);
         }
 
-        $scope.selectSearchItem = function(val){
+        $scope.selectSearchItem = function(val) {
             console.log(val);
-            if(val.type == 'Project'){
+            if (val.type == 'Project') {
                 $mdDialog.cancel();
-                $state.go('project-details', { id: val.id, name: val.name});
-            } else if(val.type =='Locality'){
+                $state.go('project-details', { id: val.id, name: val.name });
+            } else if (val.type == 'Locality') {
                 $mdDialog.cancel();
-                $state.go('project-list', {from:'search', type:'locality', id: val.id});
-            } else if(val.type =='Developer'){
+                $state.go('project-list', { from: 'search', type: 'locality', id: val.id });
+            } else if (val.type == 'Developer') {
                 $mdDialog.cancel();
-                $state.go('project-list', {from:'search', type:'developer', id: val.id});
+                $state.go('project-list', { from: 'search', type: 'developer', id: val.id });
             }
         }
     }
-
-
-
-
-    $timeout(function(){
-
-
-      var map = new google.maps.Map(document.getElementById('micro-market-map'), {
-          zoom: 5,
-          center: {lat: 24.886, lng: -70.268},
-          mapTypeId: 'terrain'
-        });
-
-        // Define the LatLng coordinates for the polygon's path.
-        var triangleCoords = [
-          {lat: 25.774, lng: -80.190},
-          {lat: 18.466, lng: -66.118},
-          {lat: 32.321, lng: -64.757},
-          {lat: 25.774, lng: -80.190}
-        ];
-
-        // Construct the polygon.
-        var bermudaTriangle = new google.maps.Polygon({
-          paths: triangleCoords,
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: '#FF0000',
-          fillOpacity: 0.35
-        });
-        bermudaTriangle.setMap(map);
-
-           },2000)
 
 })
 
@@ -190,21 +163,21 @@ app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') {
+        while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
+            return c.substring(name.length, c.length);
         }
     }
     return "";
@@ -212,8 +185,8 @@ function getCookie(cname) {
 
 
 function checkCookie(val) {
-    var data=getCookie(val);
-    if(data != ""){
+    var data = getCookie(val);
+    if (data != "") {
         // console.log(data);
         return true;
     } else {
@@ -222,8 +195,7 @@ function checkCookie(val) {
     }
 }
 
-function deleteCookie(val){
+function deleteCookie(val) {
     console.log('delete cookie called');
     setCookie(val, {}, 0);
 }
-
