d2d9746a943d918e7fee7dbb86b3a1ff47591b42
Arpit Mittal <arpit@roofpik.com>
2016-10-17 13:00:15 +0530

    home updated

--- js/controllers/home-ctrl.js
+++ js/controllers/home-ctrl.js
@@ -90,4 +90,34 @@ app.controller('homeCtrl', function($scope, $timeout, $state, $mdDialog){
         }
     }
 
+    $timeout(function(){
+
+
+      var map = new google.maps.Map(document.getElementById('micro-market-map'), {
+          zoom: 5,
+          center: {lat: 24.886, lng: -70.268},
+          mapTypeId: 'terrain'
+        });
+
+        // Define the LatLng coordinates for the polygon's path.
+        var triangleCoords = [
+          {lat: 25.774, lng: -80.190},
+          {lat: 18.466, lng: -66.118},
+          {lat: 32.321, lng: -64.757},
+          {lat: 25.774, lng: -80.190}
+        ];
+
+        // Construct the polygon.
+        var bermudaTriangle = new google.maps.Polygon({
+          paths: triangleCoords,
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF0000',
+          fillOpacity: 0.35
+        });
+        bermudaTriangle.setMap(map);
+
+           },2000)
+
 })
